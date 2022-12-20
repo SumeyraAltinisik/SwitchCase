a1f0ae486ee316a7b62201d92facce60dbf40ccb
SumeyraAltinisik <sfa2023@gmail.com>
2022-12-20 18:32:13 +0300

    Add project files.

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir harf giriniz:");
+            string harf = Console.ReadLine();
+
+            switch (harf)
+            {
+                case "a":
+                    Console.WriteLine("Ahmet, Ali, Ayşe, Aslı, Aydın");
+                    break;
+                case "b":
+                    Console.WriteLine("Betül, Büşra, Barış, Berra, Burak");
+                    break;
+                case "c":
+                    Console.WriteLine("Ceyda, Cemre, Ceren, Cenk, Cem");
+                    break;
+                case "d":
+                    Console.WriteLine("Deniz, Derya, Damla, Dilek, Dinçer");
+                    break;
+                default:
+                    Console.WriteLine("Bu karakterle uyuşan isim bulunamadı");
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
